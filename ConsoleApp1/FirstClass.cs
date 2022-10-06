ede334a6346570c5e3de79318212b0e73924f6eb
Kamolov Amir <kamolov.amir2000@yandex.ru>
2022-10-06 23:13:23 +0300

    Lecture 1

---
+++ ConsoleApp1/FirstClass.cs
@@ -0,0 +1,73 @@
+﻿namespace ConsoleApp1
+{
+    public class House
+    {
+        float Length;
+        float Height;
+        float Width;
+    }
+
+    public class Book
+    {
+        public int PageCount;
+        public string Author;
+        public string Title;
+        public string Genre;
+        public string Publisher;
+
+        // private string Description;
+
+        private string _description;
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set { 
+                if (value != null)
+                    _description = value;
+            }
+        }
+
+        internal float internalFloat;
+
+        public Book(int pageCount, string author, string title, string genre, string publisher) {
+            Console.WriteLine("Конструктор Book");
+            PageCount = pageCount;
+            Author = author;
+            Title = title;
+            Genre = genre;
+            Publisher = publisher;
+        }
+
+        public string GetDescription(bool canRead) {
+            if (canRead) { 
+                return Description;
+            }
+            return null;
+        }
+
+        public void SetDescription(string description) {
+            if (description != null)
+            {
+                Description = description;
+            }
+        }
+
+        public int GetPageCount()
+        {
+            return PageCount;
+        }
+
+        public void Increment(ref int x)
+        {
+            x = x + 1;
+        }
+
+        public void Sum(int x, int y, out int result)
+        {
+            result = x + y;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+var dom = new House();
+var dom2 = new House();
+var dom3 = new House();
+
+Book warAndPeace = new Book(150, "Толстой", "Война и мир", "Утория", "СБП");
+
+
+warAndPeace.Title = "Война и мир";
+warAndPeace.Genre = "Утопия";
+warAndPeace.PageCount = 200;
+warAndPeace.SetDescription("asdasd");
+warAndPeace.Description = "asdasdaqwe";
+warAndPeace.internalFloat = 10.23f;
+var pageCount = warAndPeace.GetPageCount();
+
+Console.WriteLine(pageCount);
+Console.WriteLine(warAndPeace.Title);
+Console.WriteLine(warAndPeace.Genre);
+Console.WriteLine(warAndPeace.PageCount);
+Console.WriteLine(warAndPeace.Description);
+Console.WriteLine();
+
+var list = new List<int>();
+list.Add(2);
+list.Add(3);
+list.Add(2);
+list.Add(4);
+
+for (var i = 0; i < list.Count; i++)
+{
+    var number = list[i];
+}
+foreach (var i in list)
+{
+    Console.WriteLine(i);
+}
+
+var dictionary = new Dictionary<int, string>();
+dictionary.Add(1, "One");
+dictionary.Add(90, "90");
+dictionary.Add(-120, "-120");
+
+Console.WriteLine(dictionary[1]);
+
+var isHasKey = dictionary.ContainsKey(-1);
+if (isHasKey)
+{
+    Console.WriteLine(dictionary[-1]);
+}
+
+var value = "";
+if (dictionary.TryGetValue(-1, out value))
+{
+    Console.WriteLine(value);
+}
+
+var x = 0;
+warAndPeace.Increment(ref x);
+Console.WriteLine(x);
+
+
+static void SetDesciption(Book book)
+{
+    book.Author = "AShdas";
+}
+
+Console.WriteLine(warAndPeace.Author);
+SetDesciption(warAndPeace);
+Console.WriteLine(warAndPeace.Author);
+
+warAndPeace.Sum(10, 20, out var result);
+Console.WriteLine(result);
